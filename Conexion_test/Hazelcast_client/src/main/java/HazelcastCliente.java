e19eec08ed74a8f8c60e03f44c9fc0ab7cf3e9a3
Javierfg2 <114407914+Javierfg2@users.noreply.github.com>
2024-07-02 12:59:15 +0100

    Add files via upload

--- Conexion_test/Hazelcast_client/src/main/java/HazelcastCliente.java
+++ Conexion_test/Hazelcast_client/src/main/java/HazelcastCliente.java
@@ -7,7 +7,9 @@ public class HazelcastCliente {
 
     public static void main(String[] args) {
         ClientConfig clientConfig = new ClientConfig();
-        clientConfig.getNetworkConfig().addAddress("192.168.43.55:5701", "192.168.43.53:5701"); // Reemplaza con las IPs de los ordenadores
+        clientConfig.getNetworkConfig().addAddress("192.168.43.221:5701", "192.168.43.53:5701",
+                "192.168.43.55" +
+                "192.168.43.166"); // Reemplaza con las IPs de los ordenadores
 
         HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
         IMap<String, String> map = client.getMap("my-distributed-map");
